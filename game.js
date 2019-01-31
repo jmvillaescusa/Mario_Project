46841e795aad73d5c31db2314373731ccc706da0
jmvillaescusa <jmvillaescusa99@gmail.com>
2019-01-31 16:31:36 -0500

    .
    Updated index.html

--- game.js
+++ game.js
@@ -6,8 +6,108 @@ $(document).ready(function () {
 	var assetsToLoad = [];
 	var assetsLoaded = 0;
 
-	var image = new Image();
-	image.addEventListener("load", loadHandler, false);
+	/*var block = new Image();
+	block.addEventListener("load", loadHandler, false);
 	block.src = "images/blocks.png";
-	enviroment.src = "enviroment.png";
+	assetsToLoad.push(block);
+
+	var enviroment = new Image();
+	enviroment.addEventListener("load", loadHandler, false);
+	enviroment.src = "images/enviroment.png";
+	assetsToLoad.push(enviroment);
+
+	var title = new Image();
+	title.addEventListener("load", loadHandler, false);
+	title.src = "images/title.png";
+	assetsToLoad.push(title);
+
+	var transition = new Image();
+	transition.addEventListener("load", loadHandler, false);
+	transition.src = "images/transition.png";
+	assetsToLoad.push(transition);*/
+
+	var TITLE = 0;
+	var LOADING = 1;
+	var PLAYING = 2;
+	var GAMEOVER = 3;
+
+	                //Keypads
+	var UP = 87;    //W
+	var LEFT = 65;  //A 
+	var DOWN = 83;  //S   
+	var RIGHT = 68; //D
+	var A = 76;     //L
+	var B = 75;     //K
+	var START = 72; //H   
+
+	var pressUp = false;
+	var pressLeft = false;
+	var pressDown = false;
+	var pressRight = false;
+	var pressA = false;
+	var pressB = false;
+	var pressStart = false;
+
+	window.addEventListener("keydown", function (event) {
+		switch (event.keyCode) {
+			case UP:
+				pressUp = true;
+				break;
+			case LEFT:
+				pressLeft = true;
+				break;
+			case DOWN:
+				pressDown = true;
+				break;
+			case RIGHT:
+				pressRight = true;
+				break;
+			case A:
+				pressA = true;
+				break;
+			case B:
+				pressB = true;
+				break;
+			case START:
+				pressStart = true;
+		}
+	}, false);
+
+	window.addEventListener("keyup", function (event) {
+		switch (event.keyCode) {
+			case UP:
+				console.log("UP");
+				pressUp = false;
+				break;
+			case LEFT:
+				pressLeft = false;
+				break;
+			case DOWN:
+				pressDown = false;
+				break;
+			case RIGHT:
+				pressRight = false;
+				break;
+			case A:
+				pressA = false;
+				break;
+			case B:
+				pressB = false;
+				break;
+			case START:
+				pressStart = false;
+		}
+	}, false);
+
+	Update();
+
+	function Update() {
+		requestAnimationFrame(Update, canvas);
+
+		Render();
+	}
+
+	function Render() {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+	}
 });
---
+++ level.js
@@ -0,0 +1,14 @@
+var Enviroment = (function (x, y, width, height) {
+	this.sourceX = 0;
+	this.sourceY = 0;
+	this.sourceWidth = 64;
+	this.sourceHeight = 64;
+
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+
+	this.image = new Image();
+	this.source = "enviroment.png";
+});
