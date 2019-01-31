795fa6a90f65ca2d57492ba53d5b55db0c0bd615
jmvillaescusa <jmvillaescusa99@gmail.com>
2019-01-31 15:01:59 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-	
+	var canvas = $("canvas");
+	var context = canvas.get(0).getContext("2d");
+
+	var sprites = [];
+	var assetsToLoad = [];
+	var assetsLoaded = 0;
+
+	var image = new Image();
+	image.addEventListener("load", loadHandler, false);
+	block.src = "images/blocks.png";
+	enviroment.src = "enviroment.png";
 });
