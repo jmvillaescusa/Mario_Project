4c1d14dd50172b0f2a5641a6556ee4488fd9f6cf
Frostiblue <46687289+Frostiblue@users.noreply.github.com>
2019-02-01 11:41:49 -0500

    added goomba

---
+++ Goomba.js
@@ -0,0 +1,24 @@
+//The sprite Object
+var Goomba = (function () {
+	this.sourceX = 0;
+	this.sourceY = 0;
+	this.sourceWidth = 16;
+	this.sourceHeight = 16;
+	this.width = 16;
+	this.height= 16;
+	this.x: = 0;
+	this.y: = 0;
+	this.vx: = 0;
+	this.vy: = 0;
+
+	var background = new Background();
+	background.image.src = "enemies.png";
+
+	var image = ;
+
+});
+
+		
+
+
+
